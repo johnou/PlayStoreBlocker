0b95325969f852248012e43e8a36be61955a9cd5
Johno Crawford <johno@sulake.com>
2025-04-11 08:54:14 +0200

    Kid got in with rapid taps - plugged the speedrun strat.

--- app/src/main/java/com/framework/accessibility/PlayStoreBlockerService.java
+++ app/src/main/java/com/framework/accessibility/PlayStoreBlockerService.java
@@ -2,23 +2,47 @@ package com.framework.accessibility;
 
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.os.Handler;
 
 public class PlayStoreBlockerService extends AccessibilityService {
 
+    private static final String EMPTY_STRING = "";
     private static final String PLAY_STORE_PACKAGE = "com.android.vending";
+    private static final int RETRY_DELAY_MS = 200;
+    private final Handler handler = new Handler();
+
+    private final Runnable retryBlocker = new Runnable() {
+        @Override
+        public void run() {
+            if (PLAY_STORE_PACKAGE.equals(String.valueOf(getActiveWindowPackage()))) {
+                performGlobalAction(GLOBAL_ACTION_BACK);
+                handler.postDelayed(this, RETRY_DELAY_MS);
+            }
+        }
+    };
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) return;
+        if (event.getEventType() != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            return;
+        }
 
-        String packageName = String.valueOf(event.getPackageName());
-        if (PLAY_STORE_PACKAGE.equals(packageName)) {
+        if (PLAY_STORE_PACKAGE.equals(String.valueOf(event.getPackageName()))) {
             performGlobalAction(GLOBAL_ACTION_BACK);
+            handler.postDelayed(retryBlocker, RETRY_DELAY_MS);
+        } else {
+            handler.removeCallbacks(retryBlocker);
         }
     }
 
     @Override
     public void onInterrupt() {
-        // Nothing needed
+        handler.removeCallbacks(retryBlocker);
+    }
+
+    private CharSequence getActiveWindowPackage() {
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        return (rootNode != null) ? rootNode.getPackageName() : EMPTY_STRING;
     }
 }
